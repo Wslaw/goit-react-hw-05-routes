4cd7ddeb542628e52f5d66a82bc8771cd9ca0290
Wslaw <114644824+Wslaw@users.noreply.github.com>
2024-01-27 21:10:58 +0200

    -2-

--- src/pages/PostsSearchPage/PostsSearchPage.jsx
+++ src/pages/PostsSearchPage/PostsSearchPage.jsx
@@ -1,9 +1,11 @@
 // import styles from './posts-search-page.module.css';
+import PostsSearch from "components/PostsSearch/PostsSearch";
 
 const PostsSearchPage = () => {
   return (
     <div>
       <h1>Posts search</h1>
+      <PostsSearch/>
     </div>
   );
 };
